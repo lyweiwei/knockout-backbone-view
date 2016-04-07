9b1188b89efd3ad05908b8ddd1394ae75b5740df
Wei Wei <lyweiwei@outlook.com>
2016-04-07 15:01:07 +0800

    add the ko backbone view.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
-define([], function () {
-  return '"knockout-backbone-view" is an awesome library.';
-});
+import Backbone from 'backbone';
+import ko from 'knockout';
+
+export default class KnockoutView extends Backbone.View {
+
+  initialize({
+    template = () => '',
+    ViewModel = class {},
+    state = {},
+    config = {},
+  }) {
+    this.template = template;
+    this.viewModel = new ViewModel(state);
+    this.config = config;
+  }
+
+  template() {
+    return this.template;
+  }
+
+  viewModel() {
+    return this.viewModel;
+  }
+
+  config() {
+    return this.config;
+  }
+
+  render() {
+    const template = this.template();
+    const config = this.config();
+
+    this.el.innerHTML = template(config);
+    ko.applyBindings(this.viewModel(), this.el);
+
+    return this;
+  }
+
+  remove() {
+    ko.clearNode(this.el);
+    super.remove();
+  }
+
+}
